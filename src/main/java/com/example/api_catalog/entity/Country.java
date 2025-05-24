ab87f4af06ffa0a0ed73d215f0b892ef9422a5c0
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-24 18:26:36 +0200

    Add @Entity and @Column to the attributes of the table Country, and empty constructor

--- src/main/java/com/example/api_catalog/entity/Country.java
+++ src/main/java/com/example/api_catalog/entity/Country.java
@@ -1,15 +1,35 @@
 package com.example.api_catalog.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "country", schema = "catalog")
 public class Country implements Serializable {
+    @Id
     private Long id;
+
+    @Column(name = "code", nullable = false, length = 4)
     private String code;
+
+    @Column(name = "name", nullable = false, length = 30)
     private String name;
+
+    @Column(name = "locale", nullable = false, length = 6)
     private String locale;
+
+    @Column(name = "time_zone", nullable = false, length = 10)
     private String timezone;
-    private Boolean enabled;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled = Boolean.TRUE;
+
+    public Country() {}
 
     public Country(Long id, String code, String name, String locale, String timezone, Boolean enabled) {
         this.id = id;
