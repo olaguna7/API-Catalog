f2467b312313ff6b058040bc9797b1fc458cf1e9
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-24 18:05:38 +0200

    Add example of a custom query in CurrencyRepository.java

--- src/main/java/com/example/api_catalog/repository/CurrencyRepository.java
+++ src/main/java/com/example/api_catalog/repository/CurrencyRepository.java
@@ -1,7 +1,9 @@
 package com.example.api_catalog.repository;
 
 import com.example.api_catalog.entity.Currency;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +12,11 @@ public interface CurrencyRepository extends CrudRepository<Currency, Long> {
     List<Currency> findByCode(String code);
     List<Currency> findByCodeAndDescription(String code, String description);
 
+    // Order queries
+    List<Currency> findByDescriptionOrderByCodeAsc(String description);
+    List<Currency> findByDescriptionOrderByCodeDesc(String description);
 
+    // Manual query
+    @Query("SELECT c FROM Currency c where c.code = :code")
+    Currency retrieveByCode(@Param("code") String code);
 }
